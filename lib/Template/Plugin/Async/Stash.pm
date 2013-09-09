00825b9ed8705d30d43e84bf8aba6b6865e2e129
Chris Andrews <chris@nodnol.org>
2013-09-09 15:22:24 +0100

    More error-handling in the Stash plugin.
    Still needs better handling of the "already fulfilled before document
    has been generated" case.

--- lib/Template/Plugin/Async/Stash.pm
+++ lib/Template/Plugin/Async/Stash.pm
@@ -11,12 +11,25 @@ sub get {
     my $ph = $self->placeholder;
     my $p = $self->context->stash->get($key);
 
-    when($p)->then(
-        sub {
-            my ($data) = @_;
-            $ph->resume($self->context, undef, $data->[0]);
-        }
-    );
+    if ($p->is_unfulfilled) {
+        when($p)->then(
+            sub {
+                my ($data) = @_;
+                $ph->resume($self->context, undef, $data->[0]);
+            },
+            sub {
+                # error - returns empty data. might be better to not
+                # run the block at all?
+                $ph->resume($self->context, undef, {});
+            }
+        );
+    }
+    else {
+        # too early to do this -- no use returning empty data, the
+        # document isn't even there to replace our placeholder in yet
+        # perhaps just abort this ASYNC block?
+        $ph->resume($self->context, undef, {});
+    }
 
     return $ph;
 }
